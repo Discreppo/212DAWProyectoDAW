60583f1b7285d622cbfe9430f356a3ce79933205
Oscar Pascual Ferrero <Discreppo@github.com>
2023-11-09 09:54:47 +0100

    Version_1.0

--- indexProyectoDAW.php
+++ indexProyectoDAW.php
@@ -1,7 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oscar Pascual Ferrero</title>
+    <style>
+        
+        body {            
+            display: flex;
+            flex-direction: column;              
+        }
+        footer{
+            bottom: 0;
+            position: fixed;
+            text-align: center;
+            width: 100%;
+            background-color: lightgray;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Despliegue de aplicaciones web</h1>
+    </header>
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+    <main>
+        <!-- Contenido principal de tu sitio web -->
+        <p>Cosas:</p>
+        <a href="doc/aunNoHayDocuments.pdf" target="_blank">Documentacion</a>
+        
+        
+    </main>
 
+    <footer>
+        <p><a href="../index.html">Oscar Pascual Ferrero</a></p>
+        <p><a href="indexProyectoDAW.php">Inicio</a> | <a href="https://github.com/discreppo" target="_blank" >GitHub</a></p>
+    </footer>
+</body>
+</html>
